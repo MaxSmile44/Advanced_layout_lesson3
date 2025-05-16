26e77718b796a025a44209bd28e471d6361ed769
MaxSmile44 <afmaxim89@gmail.com>
2025-05-16 14:46:41 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -17,24 +18,21 @@ def on_reload():
     with open('meta_data.json', 'r', encoding='utf-8') as my_file:
         meta_data_json = my_file.read()
     meta_data = json.loads(meta_data_json)
-    meta_data = list(chunked(meta_data, round(len(meta_data) / 2)))
-    rendered_page = template.render(meta_data=meta_data)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+
+    meta_data = list(chunked(meta_data, 10))
+    for i, file_data in enumerate(meta_data, start=1):
+        os.makedirs(os.path.join('.', 'pages'), exist_ok=True)
+        file_data = list(chunked(file_data, round(len(file_data) / 2)))
+        rendered_page = template.render(meta_data=file_data)
+        with open(f'./pages/index{i}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
+    # meta_data = list(chunked(meta_data, round(len(meta_data) / 2)))
+    # rendered_page = template.render(meta_data=meta_data)
+    # with open('index.html', 'w', encoding="utf8") as file:
+    #     file.write(rendered_page)
+
+on_reload()
 server.watch('template.html', on_reload)
 server.serve(root='index.html')
-
-# template = env.get_template('template.html')
-# with open('meta_data.json', 'r', encoding='utf-8') as my_file:
-#     meta_data_json = my_file.read()
-# meta_data = json.loads(meta_data_json)
-# rendered_page = template.render(meta_data=meta_data)
-# ff = list(chunked(meta_data, round(len(meta_data)/2)))
-#
-#
-# print(ff[1])
-# print(len(ff[0]))
-# print(len(ff[1]))
-# print(len(meta_data))
