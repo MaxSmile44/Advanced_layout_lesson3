a14e28a01a90929146dc8a8c3cef42616671ed9f
MaxSmile44 <afmaxim89@gmail.com>
2025-05-16 17:59:08 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -18,21 +18,15 @@ def on_reload():
     with open('meta_data.json', 'r', encoding='utf-8') as my_file:
         meta_data_json = my_file.read()
     meta_data = json.loads(meta_data_json)
-
     meta_data = list(chunked(meta_data, 10))
     for i, file_data in enumerate(meta_data, start=1):
         os.makedirs(os.path.join('.', 'pages'), exist_ok=True)
-        file_data = list(chunked(file_data, round(len(file_data) / 2)))
-        rendered_page = template.render(meta_data=file_data)
+        rendered_page = template.render(meta_data=file_data, num_pages=len(meta_data), num_page=i)
         with open(f'./pages/index{i}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
 
-    # meta_data = list(chunked(meta_data, round(len(meta_data) / 2)))
-    # rendered_page = template.render(meta_data=meta_data)
-    # with open('index.html', 'w', encoding="utf8") as file:
-    #     file.write(rendered_page)
-
 on_reload()
+
 server.watch('template.html', on_reload)
 server.serve(root='index.html')
